3b505d55a447a5dd1a4b24959d4fe571afc90ae3
dinara323 <yakubova.dinaraa@yandex.ru>
2026-09-13 00:09:59 +0300

    Creating variables and eliminating errors

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const args = process.argv.slice(2);
+
+let city;
+let days = 3;
+let noCache = false;
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === '--city') {
+    city = args[i + 1];
+  }
+
+  if (args[i] === '--days') {
+    days = Number(args[i + 1]);
+  }
+
+  if (days > 8 || days < 1 || !Number.isInteger(days)) {
+    console.error('Ошибка: параметр --days целое число от 1 до 7.');
+    process.exit(1);
+    }
+
+  if (args[i] === '--no-cache') {
+    noCache = true;
+  }
+}
+
+  if (!city) {
+    console.error('Ошибка: параметр --city обязателен.');
+    process.exit(1);
+    }
+
+console.log({
+  city,
+  days,
+  noCache,
+});
